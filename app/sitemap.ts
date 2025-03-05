114603478243a7659e763ef82f0aafa6f52afd80
Alanolog <alanrutyna2003@gmail.com>
2025-03-05 13:02:14 +0100

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,30 @@
 import type { MetadataRoute } from "next";
 const siteUrlPl = "https://www.klimatyzacja-dudek.pl/pl";
-const siteUrlEn = "https://www.klimatyzacja-dudek.pl/pl";
+const siteUrlEn = "https://www.klimatyzacja-dudek.pl/en";
 const pages = ["/wentylacja", "/klimatyzacja", "/kontakt", "/chlodnictwo", "/realizacje"];
-
+const mappedPages: {
+   url: string;
+   lastModified: Date;
+   priority: number;
+}[] = [];
+pages
+   .map((page) => [
+      {
+         url: `${siteUrlPl}${page}`,
+         lastModified: new Date(),
+         priority: 0.8,
+      },
+      {
+         url: `${siteUrlEn}${page}`,
+         lastModified: new Date(),
+         priority: 0.8,
+      },
+   ])
+   .forEach((page) => {
+      mappedPages.push(page[0]);
+      mappedPages.push(page[1]);
+   });
+console.log(mappedPages);
 export default function sitemap(): MetadataRoute.Sitemap {
    return [
       {
@@ -10,15 +32,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
          lastModified: new Date(),
          priority: 1.0,
       },
-      ...pages.map((page) => ({
-         url: `${siteUrlPl}${page}`,
-         lastModified: new Date(),
-         priority: 0.8,
-      })),
-      ...pages.map((page) => ({
-         url: `${siteUrlEn}${page}`,
-         lastModified: new Date(),
-         priority: 0.8,
-      })),
+      ...mappedPages,
    ];
 }
