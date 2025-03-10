dedb537af15d5b604176abbc9d71b7c53ad8bac4
Alanolog <alanrutyna2003@gmail.com>
2025-03-10 07:22:54 +0100

    turn off redirect on home page

--- components/layout/locale-redirect.tsx
+++ components/layout/locale-redirect.tsx
@@ -7,7 +7,7 @@ export default function LocaleRedirect({ defaultLocale }: { defaultLocale: strin
    const pathname = usePathname();
 
    useEffect(() => {
-      if (!pathname.startsWith("/pl") && !pathname.startsWith("/en")) {
+      if (!pathname.startsWith("/pl") && !pathname.startsWith("/en") && pathname !== "" && pathname !== "/") {
          if (pathname.length === 0) {
             router.replace(`/${defaultLocale}`);
          } else if (!pathname.includes("/pl") && !pathname.includes("/en")) {
