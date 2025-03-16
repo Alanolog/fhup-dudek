648cc5f947d262acbe6adb6fd61eb89d75febb40
Alanolog <alanrutyna2003@gmail.com>
2025-03-16 17:17:50 +0100

    Links in Logos

--- components/ac-components/logos grid.tsx
+++ components/ac-components/logos grid.tsx
@@ -8,17 +8,32 @@ import logoGree from "@/assets/AC_logos/logoGree.png";
 import logoVivax from "@/assets/AC_logos/logoVivax.png";
 import logoMitsubishi from "@/assets/AC_logos/logoMitsubishi.png";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 export default function LogosGrid() {
    const t = useTranslations("LogosGrid");
    return (
       <section className={styles.mainGrid}>
-         <Image src={logoHaier} width={150} alt="Haier" />
-         <Image src={logoLG} width={150} alt="LG" />
-         <Image src={logoRotenso} width={150} alt="Rotenso" />
-         <Image src={logoFuji} width={150} alt="Fuji" />
-         <Image src={logoGree} width={150} alt="Gree" />
-         <Image src={logoVivax} width={150} alt="Vivax" />
-         <Image src={logoMitsubishi} width={150} alt="Mitsubishi" />
+         <Link href="https://www.haier.com" target="_blank">
+            <Image src={logoHaier} width={150} alt="Haier" />
+         </Link>
+         <Link href="https://www.lg.com" target="_blank">
+            <Image src={logoLG} width={150} alt="LG" />
+         </Link>
+         <Link href="https://www.rotenso.com" target="_blank">
+            <Image src={logoRotenso} width={150} alt="Rotenso" />
+         </Link>
+         <Link href="https://www.fujielectric.com" target="_blank">
+            <Image src={logoFuji} width={150} alt="Fuji" />
+         </Link>
+         <Link href="https://www.gree.com" target="_blank">
+            <Image src={logoGree} width={150} alt="Gree" />
+         </Link>
+         <Link href="https://www.vivax.com" target="_blank">
+            <Image src={logoVivax} width={150} alt="Vivax" />
+         </Link>
+         <Link href="https://www.mitsubishielectric.com" target="_blank">
+            <Image src={logoMitsubishi} width={150} alt="Mitsubishi" />
+         </Link>
          <p>{t("logoMessage")}</p>
       </section>
    );
