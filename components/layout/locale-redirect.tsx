4a875d8c1b9261d7ce7a94df30ba4fdfaa26e39d
Alanolog <alanrutyna2003@gmail.com>
2025-03-16 17:23:19 +0100

    always redirect to www.

--- components/layout/locale-redirect.tsx
+++ components/layout/locale-redirect.tsx
@@ -7,6 +7,11 @@ export default function LocaleRedirect({ defaultLocale }: { defaultLocale: strin
    const pathname = usePathname();
 
    useEffect(() => {
+      const currentHost = window.location.host;
+
+      if (!currentHost.startsWith("www")) {
+         router.replace(`https://www.${window.location.host}${pathname}`);
+      }
       if (pathname.length === 0) {
          router.replace(`/${defaultLocale}`);
       }
