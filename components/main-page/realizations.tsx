b741372d90bcedfbf98dfb4b19dca0c6238f8c6c
Alanolog <alanrutyna2003@gmail.com>
2025-03-23 06:25:34 +0100

    alts in realizations

--- components/main-page/realizations.tsx
+++ components/main-page/realizations.tsx
@@ -18,31 +18,31 @@ const Realizations = () => {
          <h2>{t("title")}</h2>
          <div className={styles.imageGrid}>
             <NavLink href="/realizacje/1">
-               <Image src={realizationSmall1} alt="" />
+               <Image src={realizationSmall1} alt={t("realizationSmall1")} />
             </NavLink>
             <NavLink href="/realizacje/2">
-               <Image src={realizationSmall2} alt="" />
+               <Image src={realizationSmall2} alt={t("realizationSmall2")} />
             </NavLink>
             <NavLink href="/realizacje/3">
-               <Image src={realizationSmall3} alt="" />
+               <Image src={realizationSmall3} alt={t("realizationSmall3")} />
             </NavLink>
             <NavLink href="/realizacje/4">
-               <Image src={realizationSmall4} alt="" />
+               <Image src={realizationSmall4} alt={t("realizationSmall4")} />
             </NavLink>
             <NavLink href="/realizacje/5">
-               <Image src={realizationSmall5} alt="" />
+               <Image src={realizationSmall5} alt={t("realizationSmall5")} />
             </NavLink>
             <NavLink href="/realizacje/6">
-               <Image src={realizationSmall6} alt="" />
+               <Image src={realizationSmall6} alt={t("realizationSmall6")} />
             </NavLink>
             <NavLink href="/realizacje/7">
-               <Image src={realizationSmall7} alt="" />
+               <Image src={realizationSmall7} alt={t("realizationSmall7")} />
             </NavLink>
             <NavLink href="/realizacje/8">
-               <Image src={realizationSmall8} alt="" />
+               <Image src={realizationSmall8} alt={t("realizationSmall8")} />
             </NavLink>
             <NavLink href="/realizacje/10">
-               <Image src={realizationSmall10} alt="" />
+               <Image src={realizationSmall10} alt={t("realizationSmall10")} />
             </NavLink>
          </div>
       </section>
